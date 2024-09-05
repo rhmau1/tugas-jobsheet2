5c1ebedc8bf8f018c1c42495c68059e791a6cf3e
rahma <fijriatirahmaturrizqi@gmail.com>
2024-09-05 09:32:37 +0700

    add biodata fijriati

---
+++ Biodata10Fijriati.java
@@ -0,0 +1,15 @@
+public class Biodata10Fijriati {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("--Biodata Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t: Fijriati Rahmatur Rizqi");
+        System.out.println("NIM\t: 244107020069");
+        System.out.println("Kelas\t: TI 1H");
+        System.out.println("No Absen: 10");
+        System.out.println("Prodi\t: Teknik Informatika");
+        System.out.println("Alamat\t: JL.MT.Haryono 6a/961a");
+        System.out.println("No HP\t: 085791243799");
+        System.out.println("Hobi\t: Mendengarkan lagu, Menonton film");
+    }
+}

a8626f675cb6f29e7b7f12be0bbd8a3697c612d7
Fata-Haidar <fatahaidaraly@gmail.com>
2024-09-05 09:44:40 +0700

    Biodata fata

---
+++ biodata09Fata.java
@@ -0,0 +1,27 @@
+/**
+ * Tugas1
+ */
+public class biodata09Fata {
+    public static void main(String[] args) {
+        String name = "Fata";
+        String nim = "244107020108";
+        String klas = "1H";
+        String abs = "09";
+        String pr = "D-IV Teknik Informatika";
+        String alm = "JL. Kembang Turi 17B";
+        String hp = "081259818891";
+        String hob = "Baca Manhwa";
+         System.out.println("-------------------------------------------------------");
+            System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+            System.out.println("-------------------------------------------------------");
+        System.out.println("Nama     : "+ name);
+        System.out.println("NIM      : "+ nim);
+        System.out.println("Kelas    : "+ klas);
+        System.out.println("No Absen : "+ abs);
+        System.out.println("Prodi    : "+ pr);
+        System.out.println("Alamat   : "+ alm);
+        System.out.println("No HP    : "+ hp);
+        System.out.println("Hobi     : "+ hob);
+    }
+        
+    }
